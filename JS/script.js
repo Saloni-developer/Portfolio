b0cd748c613f2f926958a43900b7c048269d05fb
Saloni-developer <saloniagg653@gmail.com>
2024-02-06 16:18:22 +0530

    save changes

--- JS/script.js
+++ JS/script.js
@@ -1,24 +1,7 @@
-// const flip=document.getElementById("flip");
+function myFunction() {
+    var element = document.getElementById("myDIV");
+    var navitem = document.getElementById("nav")
+    element.classList.toggle("hamburgerClose");
+    nav.classList.toggle("mystyle");
 
-
-// const addlist=()=>{
-
-//    const list=document.createElement("div");
-//    list.classList.toggle("navicons");
-   
-//    const htmlData=`
-//              <ul>
-//                 <li><a href="index.html" >Home</a></li>
-//                 <li><a href="#about">About</a></li>
-//                 <li><a href="">Portfolio</a></li>
-//                 <li><a href="#contact">Contact</a></li>
-//             </ul>`;
-
-//     list.insertAdjacentHTML("afterbegin",htmlData);  
-//     // console.log(list);  
-//     document.body.appendChild(list);
-    
-// }
-
-// flip.addEventListener('click',addlist);
-
+}
